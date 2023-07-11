8391ff6ddfd8f1f1de0e5c771dc6aa4acb9688f3
Roman Drabusheuski <133542929+Roman-marto@users.noreply.github.com>
2023-07-11 12:21:23 +0300

    Add files via upload

---
+++ Task312.java
@@ -0,0 +1,8 @@
+public class Task312 {
+    public static void main (String[] args) {
+        String a = "word of the incident spread quickly";
+        String b = a.replace("word", "letter");
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
